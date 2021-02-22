aead6db41a3665ed7b8997a5b0cba2afc3a6c0d5
Victor Matyzhonok <victor@matyzhonok.ru>
2021-02-22 03:20:10 +0300

    Добавил в API main/status статусы света обоих этажей

--- API_Info.ino
+++ API_Info.ino
@@ -1,12 +1,27 @@
 void request_status(WiFiClient client) {
-  String str;
-  LED_Status = get_BUILTIN_LED_Status();
-  String timeAfterStartStr = millis_ToString(millis());
-  if (LED_Status){
-    str = "Светодиод ВКЛЮЧЁН. (<a href='http://" + MyIP + "/LED=OFF'>выключить</a>)<br />";
+  String str = String("{\"BuiltIn_Led status\": \"");
+  if (get_BUILTIN_LED_Status()) {
+    str = str + String("On");
   } else {
-    str = "Светодиод ВЫКЛЮЧЁН. (<a href='http://" + MyIP + "/LED=ON'>включить</a>)<br />";
-  };
-  str = str + "<p align=\"right\">Времени с начала работы: " + timeAfterStartStr + "</p>";
-  print_to_web(str, client, true);
+    str = str + String("Off");
+  }
+  str = str + "\", ";
+  
+  str = str + String("\"Floor_1_LED status\": \"");
+  if (get_Floor_1_Led_Status()) {
+    str = str + String("On");
+  } else {
+    str = str + String("Off");
+  }
+  str = str + "\", ";
+
+  str = str + String("\"Floor_2_LED status\": \"");
+  if (get_Floor_2_Led_Status()) {
+    str = str + String("On");
+  } else {
+    str = str + String("Off");
+  }
+    
+  str = str + String("\"}");
+  print_to_web(str, client, false); 
 }
